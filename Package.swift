cbc7990162902557d9be3a252f4ce6c34116e4bc
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2021-01-15 18:22:55 -0600

    Added Datable dependency

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Transmission",
-            dependencies: ["Chord"]),
+            dependencies: ["Chord", "Datable"]),
         .target(
             name: "TransmissionLinux",
             dependencies: ["Chord", "Socket"]),
