f19c4784d14b90069a95ed6c8e0921545c09636f
consuelita <adelita@operatorfoundation.org>
2021-04-28 18:17:41 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Transmission",
-    platforms: [.macOS(.v10_15), .iOS(.v13)],
+    platforms: [.macOS(.v11), .iOS(.v14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.0.5"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.0.11"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.0.3"),
         .package(url: "https://github.com/OperatorFoundation/Transport", from: "2.3.3"),
     ],
