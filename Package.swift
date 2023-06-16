253f38b1e5b417d23973e3209419b2429cc1309f
Jessica9-star <68959096+Jessica9-star@users.noreply.github.com>
2023-06-16 17:28:29 -0500

    Updated Package Version to .iOS(.v16)

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     name: "Transmission",
     platforms: [
         .macOS(.v13),
-        .iOS(.v15)
+        .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
